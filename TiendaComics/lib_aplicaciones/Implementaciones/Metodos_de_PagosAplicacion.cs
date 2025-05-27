243796e10eab72ae7c882a352dda026ac64e5319
BranPetit <santiagobran34@gmail.com>
2025-05-26 21:01:10 -0500

    Implementaciones metodos de pago, roles y sucursales

---
+++ TiendaComics/lib_aplicaciones/Implementaciones/Metodos_de_PagosAplicacion.cs
@@ -0,0 +1,110 @@
+﻿using lib_aplicaciones.Interfaces;
+using lib_dominio.Entidades;
+using lib_dominio.Nucleo;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace lib_aplicaciones.Implementaciones
+{
+    public class Metodos_de_PagosAplicacion : IMetodos_de_PagosAplicacion
+    {
+        private IConexion? IConexion = null;
+        private IAuditoriasAplicacion? IAuditoriasAplicacion = null;
+
+        public Metodos_de_PagosAplicacion(IConexion iConexion, IAuditoriasAplicacion iAuditoriasAplicacion)
+        {
+            this.IConexion = iConexion;
+            this.IAuditoriasAplicacion = iAuditoriasAplicacion;
+        }
+
+        public void Configurar(string StringConexion)
+        {
+            this.IConexion!.StringConexion = StringConexion;
+        }
+
+        public Metodos_de_Pagos? Borrar(Metodos_de_Pagos? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.Id == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Calculos
+
+            this.IConexion!.Metodos_de_Pagos!.Remove(entidad);
+            this.IConexion.SaveChanges();
+            this.IAuditoriasAplicacion!.Configurar(this.IConexion.StringConexion!);
+            this.IAuditoriasAplicacion!.Guardar(new Auditorias
+            {
+                Usuario = "admin",
+                Entidad = "Metodos de Pagos",
+                Operacion = "Borrar",
+                Datos = JsonConversor.ConvertirAString(entidad!),
+                Fecha = DateTime.Now
+            });
+            return entidad;
+        }
+
+        public Metodos_de_Pagos? Guardar(Metodos_de_Pagos? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad.Id != 0)
+                throw new Exception("lbYaSeGuardo");
+
+            // Calculos
+
+            this.IConexion!.Metodos_de_Pagos!.Add(entidad);
+            this.IConexion.SaveChanges();
+            this.IAuditoriasAplicacion!.Configurar(this.IConexion.StringConexion!);
+            this.IAuditoriasAplicacion!.Guardar(new Auditorias
+            {
+                Usuario = "admin",
+                Entidad = "Metodos de Pagos",
+                Operacion = "Guardar",
+                Datos = JsonConversor.ConvertirAString(entidad!),
+                Fecha = DateTime.Now
+            });
+            return entidad;
+        }
+
+        public List<Metodos_de_Pagos> Listar()
+        {
+            return this.IConexion!.Metodos_de_Pagos!.Take(20).ToList();
+        }
+
+        public List<Metodos_de_Pagos> PorNombre(Metodos_de_Pagos? entidad)
+        {
+            return this.IConexion!.Metodos_de_Pagos!
+                .Where(x => x.Nombre!.Contains(entidad!.Nombre!))
+                .ToList();
+        }
+
+        public Metodos_de_Pagos? Modificar(Metodos_de_Pagos? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.Id == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Calculos
+
+            var entry = this.IConexion!.Entry<Metodos_de_Pagos>(entidad);
+            entry.State = EntityState.Modified;
+            this.IConexion.SaveChanges();
+            this.IAuditoriasAplicacion!.Configurar(this.IConexion.StringConexion!);
+            this.IAuditoriasAplicacion!.Guardar(new Auditorias
+            {
+                Usuario = "admin",
+                Entidad = "Metodos de Pagos",
+                Operacion = "Modificar",
+                Datos = JsonConversor.ConvertirAString(entidad!),
+                Fecha = DateTime.Now
+            });
+            return entidad;
+        }
+    }
+}
---
+++ TiendaComics/lib_aplicaciones/Implementaciones/SucursalesAplicacion.cs
@@ -0,0 +1,110 @@
+﻿using lib_aplicaciones.Interfaces;
+using lib_dominio.Entidades;
+using lib_dominio.Nucleo;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace lib_aplicaciones.Implementaciones
+{
+    public class SucursalesAplicacion : ISucursalesAplicacion
+    {
+        private IConexion? IConexion = null;
+        private IAuditoriasAplicacion? IAuditoriasAplicacion = null;
+
+        public SucursalesAplicacion(IConexion iConexion, IAuditoriasAplicacion iAuditoriasAplicacion)
+        {
+            this.IConexion = iConexion;
+            this.IAuditoriasAplicacion = iAuditoriasAplicacion;
+        }
+
+        public void Configurar(string StringConexion)
+        {
+            this.IConexion!.StringConexion = StringConexion;
+        }
+
+        public Sucursales? Borrar(Sucursales? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.Id == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Calculos
+
+            this.IConexion!.Sucursales!.Remove(entidad);
+            this.IConexion.SaveChanges();
+            this.IAuditoriasAplicacion!.Configurar(this.IConexion.StringConexion!);
+            this.IAuditoriasAplicacion!.Guardar(new Auditorias
+            {
+                Usuario = "admin",
+                Entidad = "Sucursales",
+                Operacion = "Borrar",
+                Datos = JsonConversor.ConvertirAString(entidad!),
+                Fecha = DateTime.Now
+            });
+            return entidad;
+        }
+
+        public Sucursales? Guardar(Sucursales? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad.Id != 0)
+                throw new Exception("lbYaSeGuardo");
+
+            // Calculos
+
+            this.IConexion!.Sucursales!.Add(entidad);
+            this.IConexion.SaveChanges();
+            this.IAuditoriasAplicacion!.Configurar(this.IConexion.StringConexion!);
+            this.IAuditoriasAplicacion!.Guardar(new Auditorias
+            {
+                Usuario = "admin",
+                Entidad = "Sucursales",
+                Operacion = "Guardar",
+                Datos = JsonConversor.ConvertirAString(entidad!),
+                Fecha = DateTime.Now
+            });
+            return entidad;
+        }
+
+        public List<Sucursales> Listar()
+        {
+            return this.IConexion!.Sucursales!.Take(20).ToList();
+        }
+
+        public List<Sucursales> PorNombre(Sucursales? entidad)
+        {
+            return this.IConexion!.Sucursales!
+                .Where(x => x.Nombre!.Contains(entidad!.Nombre!))
+                .ToList();
+        }
+
+        public Sucursales? Modificar(Sucursales? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.Id == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Calculos
+
+            var entry = this.IConexion!.Entry<Sucursales>(entidad);
+            entry.State = EntityState.Modified;
+            this.IConexion.SaveChanges();
+            this.IAuditoriasAplicacion!.Configurar(this.IConexion.StringConexion!);
+            this.IAuditoriasAplicacion!.Guardar(new Auditorias
+            {
+                Usuario = "admin",
+                Entidad = "Sucursales",
+                Operacion = "Modificar",
+                Datos = JsonConversor.ConvertirAString(entidad!),
+                Fecha = DateTime.Now
+            });
+            return entidad;
+        }
+    }
+}
