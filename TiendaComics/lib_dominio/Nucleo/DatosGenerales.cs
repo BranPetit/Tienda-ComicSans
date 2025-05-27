f5b5f23127b3271631d15af6a2df7d7350e775f8
BranPetit <santiagobran34@gmail.com>
2025-05-26 20:15:26 -0500

    Agregacion clases de Nucleo y nugget de newtonsoft

---
+++ TiendaComics/lib_dominio/Nucleo/DatosGenerales.cs
@@ -0,0 +1,10 @@
+﻿namespace lib_dominio.Nucleo
+{
+    public class DatosGenerales
+    {
+        public static string ruta_json = @"C:\Users\santi\Downloads\8-10 Mar-Jue Santiago Bran-Alexsander Gonzalez\8-10 Mar-Jue Santiago Bran-Alexsander Gonzalez\TiendaComics\secrets.json";
+        public static bool usa_azure = false;
+        public static string clave = "EVBgi345936456ghhVBJGtgnifytsidi3456678jhgUTytutyiiyi";
+        public static string usuario_datos = EncriptarConversor.Encriptar("Test.Trghhjsgdj");
+    }
+}
--- TiendaComics/lib_dominio/obj/Debug/net8.0/lib_dominio.AssemblyInfo.cs
+++ TiendaComics/lib_dominio/obj/Debug/net8.0/lib_dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e62c0a45a37598f6b972f3ad5a5cae0032f6efce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a32303c2fe59bd5bde4d6c0265426435b87330")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
