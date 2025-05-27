d43a1211620eda354de118682c8f9d67a0ae0902
BranPetit <santiagobran34@gmail.com>
2025-05-26 20:40:22 -0500

    Agregacion IConexion, dependecias de dominio y nugget SQLSERVER

--- TiendaComics/lib_dominio/obj/Debug/net8.0/lib_dominio.AssemblyInfo.cs
+++ TiendaComics/lib_dominio/obj/Debug/net8.0/lib_dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b5f23127b3271631d15af6a2df7d7350e775f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d25815159510ff6104868940c1a83ddfbcee33")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TiendaComics/lib_repositorios/Interfaces/IConexion.cs
@@ -0,0 +1,24 @@
+﻿using lib_dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+namespace lib_repositorios.Interfaces
+{
+    public interface IConexion
+    {
+        string? StringConexion { get; set; }
+        DbSet<Categorias>? Categorias { get; set; }
+        DbSet<Editoriales>? Editoriales { get; set; }
+        DbSet<Comics>? Comics { get; set; }
+        DbSet<Clientes>? Clientes { get; set; }
+        DbSet<Vendedores>? Vendedores { get; set; }
+        DbSet<Ventas>? Ventas { get; set; }
+        DbSet<Detalles_Ventas>? Detalles_Ventas { get; set; }
+        DbSet<Metodos_de_Pagos>? Metodos_de_Pagos { get; set; }
+        DbSet<Sucursales>? Sucursales { get; set; }
+        DbSet<Auditorias> Auditorias { get; set; }
+        DbSet<Roles> Roles { get; set; }
+        DbSet<Usuarios> Usuarios { get; set; }
+        EntityEntry<T> Entry<T>(T entity) where T : class;
+        int SaveChanges();
+    }
+}
